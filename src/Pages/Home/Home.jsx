5206956c12d15cf2e2e06140bb34bb3ea2823cc2
arafat-sabbir <arafatshabbir8@gmail.com>
2023-12-22 17:55:58 +0600

    added userbase or relevent for people section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import Banner from "./Banner/Banner";
+import Footer from "./Footer/Footer";
+import OurUser from "./OurUser/OurUser";
 
 
 const Home = () => {
     return (
         <div>
                 <Banner></Banner>
+                <OurUser></OurUser>
+                <Footer></Footer>
         </div>
     );
 };
